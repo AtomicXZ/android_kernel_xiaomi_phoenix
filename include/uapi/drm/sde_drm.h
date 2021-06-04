sm6150-dev/android_kernel_xiaomi_sm6150

drm: Rewrite fod handling based off Xiaomi's implementation
Change-Id: I3948d6dbb72176da8a06788a65a4a91d98dd2170

include/uapi/drm/sde_drm.h
@@ -495,4 +495,10 @@ struct sde_drm_roi_misr_v1 {
 #define SDE_RECOVERY_CAPTURE		1
 #define SDE_RECOVERY_HARD_RESET		2
 
+/**
+ * sde fod zpos value for notifying client
+ */
+#define FOD_ICON_LAYER_ZORDER    0x1000000u
+#define FOD_PRESSED_LAYER_ZORDER 0x2000000u
+
 #endif /* _SDE_DRM_H_ */

AtomicXZ/android_kernel_xiaomi_phoenix

include/uapi/drm/sde_drm.h
@@ -495,4 +495,10 @@ struct sde_drm_roi_misr_v1 {
 #define SDE_RECOVERY_CAPTURE		1
 #define SDE_RECOVERY_HARD_RESET		2
 
+/**
+ * sde fod zpos value for notifying client
+ */
+#define FOD_HBM_LAYER_ZORDER     0x1000000u
+#define FOD_ICON_LAYER_ZORDER    0x2000000u
+
 #endif /* _SDE_DRM_H_ */